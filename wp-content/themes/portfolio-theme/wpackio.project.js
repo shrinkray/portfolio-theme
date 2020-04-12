de5d79d17b295208fb52ab272e9566a7199a4447
Greg <hello@gregmiller.io>
2020-04-12 10:15:41 -0400

    Update settings in wpack.project

--- wp-content/themes/portfolio-theme/wpackio.project.js
+++ wp-content/themes/portfolio-theme/wpackio.project.js
@@ -66,12 +66,13 @@ module.exports = {
 	// Won't hurt because we use PHP to automate loading
 	optimizeSplitChunks: true,
 	// Usually PHP and other files to watch and reload when changed
-	watch: './inc|includes/**/*.php',
+	watch: './lib/**/*.php',
 	// Files that you want to copy to your ultimate theme/plugin package
 	// Supports glob matching from minimatch
 	// @link <https://github.com/isaacs/minimatch#usage>
 	packageFiles: [
-		'inc/**',
+		'lib/**',
+		'assets/**',
 		'vendor/**',
 		'dist/**',
 		'*.php',
